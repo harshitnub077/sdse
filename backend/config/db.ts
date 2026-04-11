cd0159a8efea2b5e9e83c2e64aed37cb212d42be
Prakhar13o3 <rawatprakhar13o3@gmail.com>
2026-04-11 12:00:00 +0530

    feat: Implement backend configuration and models

--- backend/config/db.ts
+++
@@ -1,7 +0,0 @@
-import * as mongoose from "mongoose";
-
-export const connectDB = async () => {
-  await (mongoose as any)
-    .connect(process.env.MONGO_URL ?? "")
-    .then(() => console.log("DB Connected"));
-};
